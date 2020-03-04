026bdb1433d9b7c3857073aa432e2da7fb915f24
雨季不再来 <yuanyu1909@163.com>
2020-03-04 14:41:52 +0800

    commit

---
+++ src/components/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-button
+      type="info"
+      @click="exit"
+    >
+      退出
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    exit () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style scoped lang=""></style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入图标库
+import './assets/fonts/iconfont.css'
+// 导入全局样式
+import './assets/css/global.css'
 
+// 配置axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// console.log(Vue.propotype.$http)
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
